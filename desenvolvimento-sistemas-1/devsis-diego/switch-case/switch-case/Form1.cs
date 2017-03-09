9ad57167e0f2ef9311e604eccdbafc76d7d3fbfa
unknown <leal.banks@gmail.com>
2017-03-09 16:28:47 -0300

    pull repository

---
+++ desenvolvimento-sistemas-1/devsis-diego/switch-case/switch-case/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace switch_case
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxMeses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String mes = comboBoxMeses.Text;
+            int numMes = comboBoxMeses.SelectedIndex;
+            textMes.Text = numMes.ToString();
+
+
+
+        }
+    }
+}
