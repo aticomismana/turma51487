da4a4e995f69b885c06bb69606834a5eeeea0e65
unknown <leal.banks@gmail.com>
2017-03-09 13:59:30 -0300

    Variaveis e tipos primitivos

---
+++ desenvolvimento-sistemas-1/devsis-diego/tipos-primitivos/tipos-primitivos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipos_primitivos
+{
+    class Program
+    {
+        String hello = "Olá mundo, desgraça";
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+
+            Console.WriteLine(pro.hello);
+
+            Console.ReadLine();
+        }
+    }
+}
