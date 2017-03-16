03849e9e7eb227fb33044e35237fa70c041fcc12
thiagoN13 <thiago.nogueira187@gmail.com>
2017-03-16 16:45:41 -0300

    Estrutura de repeticao

---
+++ desenvolvimento-sistemas-1/exercicios/devsis-thiago/estruturaRepeticao/estruturaRepeticao/While.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace estruturaRepeticao
+{
+    partial class While
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Repetição While";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Voltar ao menu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // While
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "While";
+            this.Text = "@while";
+            this.Load += new System.EventHandler(this.while_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ desenvolvimento-sistemas-1/exercicios/devsis-thiago/estruturaRepeticao/estruturaRepeticao/While.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estruturaRepeticao
+{
+    public partial class While : Form
+    {
+        public While()
+        {
+            InitializeComponent();
+        }
+
+        private void while_Load(object sender, EventArgs e)
+        {
+            int i = 15;
+            double dinheiro = 1000.0;
+            while(i >= 15)
+            {
+                dinheiro -= i;
+                i--;
+            }
+            MessageBox.Show("O dinheiro subtraido " + dinheiro);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+
+            do
+            {
+                MessageBox.Show(i.ToString());
+                i++;
+            } while (i < 5);
+        }
+    }
+}
