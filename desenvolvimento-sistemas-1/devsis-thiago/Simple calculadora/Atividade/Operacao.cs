4cf058a309a896fc0d882d21f97c89fe2776df20
Thiago <Thiago.nogueira187@gmail.com>
2017-03-30 15:48:08 -0300

    Simple calculadora in C#

---
+++ desenvolvimento-sistemas-1/devsis-thiago/Simple calculadora/Atividade/Operacao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Atividade
+{
+     class Operacao
+    {
+        public double resultado;
+
+        public double soma(double num2, double num1)
+        {
+            resultado = num1 + num2;
+            return resultado;
+        }
+
+        public double multiplicacao(double num1, double num2)
+        {
+            resultado = num1 * num2;
+            return resultado;
+        }
+
+        public double divisao(double num1, double num2)
+        {
+            resultado = num1 / num2;
+            return resultado;
+        }
+
+        public double subtracao(double num1, double num2)
+        {
+            resultado = num1 - num2;
+            return resultado;
+        }
+
+        public static implicit operator int(Operacao v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
