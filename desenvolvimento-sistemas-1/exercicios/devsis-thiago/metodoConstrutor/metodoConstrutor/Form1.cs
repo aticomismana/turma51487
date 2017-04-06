a47499e033f483dadc07346b02897e17f95d0493
unknown <leal.banks@gmail.com>
2017-04-06 16:57:31 -0300

    Metodo construtor

---
+++ desenvolvimento-sistemas-1/exercicios/devsis-thiago/metodoConstrutor/metodoConstrutor/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace metodoConstrutor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cadastrar_Click(object sender, EventArgs e)
+        {
+            
+            Usuario user = new Usuario(Convert.ToString(txtNome.Text), Convert.ToString(txtCidade.Text), Convert.ToString(txtEstado.Text),Convert.ToInt32(txtCPF.Text), Convert.ToInt32(txtRG.Text));
+
+            MessageBox.Show(user.nomeCompleto);
+
+        }
+    }
+}
---
+++ desenvolvimento-sistemas-1/exercicios/devsis-thiago/metodoConstrutor/metodoConstrutor/Usuario.cs
@@ -0,0 +1,25 @@
+﻿namespace metodoConstrutor
+{
+
+    public class Usuario
+    {
+        public string nomeCompleto { get; set; }
+        public string cidade { get; set; }
+        public string estado { get; set; } 
+        private int cpf { get; set; }
+        private int rg { get; set; }
+        
+
+        public Usuario(string nome, string cidade, string estado, int cpf, int rg)
+        {
+            this.nomeCompleto = nome;
+            this.cidade = cidade;
+            this.estado = estado;
+            this.cpf = cpf;
+            this.rg = rg;
+            this.nomeCompleto = nome;
+
+        }
+
+    }
+}
