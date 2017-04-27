8632756522bf7bf481b13186f8e80d52c4aaa431
Nayrivis <Nayr Araujo>
2017-04-27 16:45:06 -0300

    Atividade aula 27/04

---
+++ desenvolvimento-sistemas-1/exercicios/devsis-thiago/atividade27-04/ConsoleApplication2/Diretor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Diretor : Funcionario
+    {
+        public string usuario;
+        public string senha;
+
+        public new void Bonificacao()
+        {
+            salario = salario + salario * 20 / 100;
+            Console.Write("Novo salario do direitor: " + salario);
+        }
+    }
+}
