2c695666539d9b53dfb1b6730fce7786e32028a9
Ático Mismana <aticomismana7@gmail.com>
2017-08-02 14:42:15 -0300

    implementa HelloWorld com e sem IDE

---
+++ desenvolvimento-sistemas-2/exercicios/2017.08.02_revisao-java/mismana/HelloWord/src/helloword/HelloWord.java
@@ -0,0 +1,11 @@
+package helloword;
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("java is hard!!!!");
+    }
+    
+}
+
---
+++ desenvolvimento-sistemas-2/exercicios/2017.08.02_revisao-java/mismana/HelloWorld_without_IDE.java
@@ -0,0 +1,5 @@
+public class HeloWorld {
+	public static void main (String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
