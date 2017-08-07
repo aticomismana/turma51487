c8a45fcebf38e6fbd48d0a34a052eff082a4b84e
Ático Mismana <aticomismana7@gmail.com>
2017-08-07 15:35:03 -0300

    remove metodo main de classe default do projeto

--- desenvolvimento-sistemas-2/exercicios/2017.08.07_revisao-00/mismana/prj-conta1/src/prj/conta1/PrjConta1.java
+++ desenvolvimento-sistemas-2/exercicios/2017.08.07_revisao-00/mismana/prj-conta1/src/prj/conta1/PrjConta1.java
@@ -1,9 +1,3 @@
 package prj.conta1;
 
-public class PrjConta1 {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
-        // run class test with Shift+F6 in NetBeans
-    }
-}
+public class PrjConta1 {}
