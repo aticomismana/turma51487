8508b3eae3f3858eb22c739e36aecd46022b5692
thiago_n13 <thiago.nogueira187@gmail.com>
2017-09-15 11:10:45 +0000

    Subindo alteracoes com web sql

--- desenvolvimento-sistemas-2/exercicios/2017.08.29_atividade-formulario-html/thiago/atividade/websql.js
+++ desenvolvimento-sistemas-2/exercicios/2017.08.29_atividade-formulario-html/thiago/atividade/websql.js
@@ -1,1 +1,65 @@
+// Criando banco local de 5mb de armazenamento
 var db = window.openDatabase("bancoLocal", "1.0", "Teste banco local", 5*1024*1024);
+
+// Carga inicial
+criarTabelas();
+buscarAlunos();
+
+function submeterFormulario(){
+  var
+    nome = $("#nome").val(),
+    email = $("#email").val(),
+    senha = $("#senha").val(),
+    escola = $("#escola").val(),
+    data_nascimento = $("#data_nascimento").val();
+
+  var aluno = [nome, email, senha, escola, data_nascimento];
+
+  db.transaction (function (tx) {
+    tx.executeSql ("INSERT INTO aluno(nome, email, senha, escola, data_nascimento) VALUES (?, ?, ?, ?, ?) ", aluno,
+      function(success){
+        window.location.href = "lista.html";
+      }, null);
+  });
+
+}
+
+function apagarRegistro(indice){
+  db.transaction (function (tx) {
+    tx.executeSql ("DELETE FROM aluno WHERE id = ?", [indice], null, null);
+    window.location.href = "lista.html";
+  })
+}
+
+
+function buscarAlunos(){
+  db.transaction (function (tx) {
+    tx.executeSql ('SELECT * FROM aluno', [], function (tx, results) {
+      var len = results.rows.length;
+      for (var i = 0; i <len; i ++) {
+        var newRow = $("<tr>");
+        var cols = "";
+
+        cols += '<td>' + results.rows.item(i).nome + '</td>';
+        cols += '<td>' + results.rows.item(i).escola + '</td>';
+        cols += '<td>' + results.rows.item(i).data_nascimento + '</td>';
+        cols += '<td>' + results.rows.item(i).email + '</td>';
+        cols += '<td><button type="button" name="remover" onClick="apagarRegistro(' + results.rows.item(i).id + ')" class="btn btn-danger">Apagar</button></td>';
+        cols += '<td>';
+        cols += '</td>';
+
+        newRow.append(cols);
+        $("#tabela-alunos").append(newRow);
+      }
+    }, null);
+  });
+}
+
+function criarTabelas(){
+  db.transaction (function (tx) {
+    tx.executeSql ("CREATE TABLE IF NOT EXISTS aluno (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, senha TEXT, escola TEXT, data_nascimento TEXT)");
+
+    // tx.executeSql ("DROP TABLE aluno");
+
+  });
+}
