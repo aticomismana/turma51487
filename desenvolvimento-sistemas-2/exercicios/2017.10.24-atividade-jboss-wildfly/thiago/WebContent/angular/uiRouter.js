4d6a53166c92643f60cade3b7d44a1705fb1aef4
thagoN13 <thago.nogueira187@gmail.com>
2017-10-24 19:49:04 -0300

    Subindo atividade com o jboss

---
+++ desenvolvimento-sistemas-2/exercicios/2017.10.24-atividade-jboss-wildfly/thiago/WebContent/angular/uiRouter.js
@@ -0,0 +1,25 @@
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  // Now set up the states
+  $stateProvider
+    .state('index', {
+      url: "/",
+      templateUrl: "partials/container.html"
+    })
+    .state('list', {
+      url: "/list",
+      templateUrl: "partials/list.html",
+      controller: "ListController"
+    })
+    .state('form', {
+      url: "/form",
+      templateUrl: "partials/todoForm.html",
+      controller: "TodoFormController"
+    })
+    .state('atividadeForm', {
+      url: "/atividadeForm",
+      templateUrl: "partials/atividadeForm.html",
+      controller: "AtividadeFormController"
+    })
+});
