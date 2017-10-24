72874a49ec7b6ed95f4f0fbdf1d44c520ee6d4ff
Fran Almeida <fran.almeida1990@gmail.com>
2017-10-24 14:22:27 -0300

    Código bugado

---
+++ desenvolvimento-sistemas-2/exercicios/2017.10.23-atividade-json/Fran/js/pessoaController.js
@@ -0,0 +1,6 @@
+var agenda = angular.module('agenda', []);
+agenda.controller('agendaController', function($scope, $http) {
+  $http.get('js/contatos.json').then(function(response) {
+    $scope.contatos = response.data.contatos;
+  }) ;     
+});
