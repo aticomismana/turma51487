a07fe2ee37a236a0bc131b5a5b954808ace8608b
Ático Mismana <aticomismana7@gmail.com>
2017-10-31 16:51:19 -0300

    cria atividade

---
+++ desenvolvimento-sistemas-2/exercicios/2017.10.31_atividade-json-java/mismana/src/facade/PessoaFacade.java
@@ -0,0 +1,30 @@
+package facade;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Pessoa;
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML})
+
+@Consumes({MediaType.APPLICATION_JSON})
+
+@Path("/pessoas")
+public class PessoaFacade {
+	static ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+	
+	static {
+		pessoas.add(new Pessoa("Fran", "12345678", 26));
+		pessoas.add(new Pessoa("David", "54214567", 19));
+		pessoas.add(new Pessoa("Mouse", "12346625", 15));
+	}
+	
+	@GET
+	public ArrayList<Pessoa> getPessoaFacade() {
+		return pessoas;
+	}
+}
